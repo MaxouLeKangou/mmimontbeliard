0907cafd88f589fc177f11d6667d2c0f97208e5f
Maxime LEGRAND <maxime.legrand111@outlook.com>
2023-12-20 11:37:58 +0100

    feat. Add meta description

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,7 +18,7 @@ export default defineNuxtConfig({
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { hid: 'description', name: 'description', content: '' },
+        { hid: 'description', name: 'description', content: "Explorez l'univers numérique au Département MMI. Formation de pointe en multimédia, internet, développement web, design, et plus encore. Préparez-vous pour l'avenir du digital avec nous." },
         { name: 'format-detection', content: 'telephone=no' },
       ],
       link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
