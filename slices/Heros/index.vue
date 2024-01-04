f5545569b51a54d60af9f3addf37516125d63db6
Maxime LEGRAND <maxime.legrand111@outlook.com>
2024-01-04 20:45:51 +0100

    fix. Link crawlable

--- slices/Heros/index.vue
+++ slices/Heros/index.vue
@@ -30,9 +30,9 @@ const scrollDown = () => {
     <div class="px-[6%] xl:px-[10%] flex flex-col gap-5 justify-center text-center items-center h-[100vh] lg:gap-10 py-10">
       <PrismicRichText :field="slice.primary.title" class="max-w-max font-marker bg-gradient-to-r from-purple to-pink text-transparent bg-clip-text text-3xl lg:text-7xl"/>
       <PrismicRichText :field="slice.primary.content" class="font-normal text-light-100 text-sm max-w-[600px] lg:text-xl"/>
-      <a class="inline-block bg-dark-200 text-light-100 rounded-xl border border-width-2 border-dark-100 p-0.5 mt-3 lg:mt-5 hover:border-light-200">
+      <div class="inline-block bg-dark-200 text-light-100 rounded-xl border border-width-2 border-dark-100 p-0.5 mt-3 lg:mt-5 hover:border-light-200">
         <button @click="scrollDown" class="cursor-pointer py-3 px-6 text-sm lg:text-base lg:py-3 lg:px-10">en découvrir plus</button>
-      </a>
+      </div>
     </div>
   </section>
 </template>
