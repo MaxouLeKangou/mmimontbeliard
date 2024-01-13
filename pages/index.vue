850febaab5b950789528c046ee8a64d1ae06efd1
Maxime LEGRAND <maxime.legrand111@outlook.com>
2024-01-13 16:35:49 +0100

    fix. Remove ThreeJS Code (doesn't work)

--- pages/index.vue
+++ pages/index.vue
@@ -12,60 +12,6 @@
     title: prismic.asText(page.value?.data.title)
   })
 
-  // import { Scene } from 'three';
-  // import * as THREE from 'three';
-
-  // let renderer: THREE.WebGLRenderer;
-  // const experience: Ref<HTMLCanvasElement | null> = ref(null);
-
-  // const { width, height } = useWindowSize();
-  // const aspectRatio = computed(() => width.value / height.value);
-
-
-  // const bgColor = new THREE.Color(0x0C0C0D);
-  // const scene = new Scene();
-  // scene.background = bgColor;
-
-  // const camera = new THREE.PerspectiveCamera( 75,  aspectRatio.value, 0.1, 1000 );
-  // camera.position.set(0, 0, 2);
- 
-  // scene.add(camera);
-
-  // const sphere = new THREE.Mesh(
-  //   new THREE.SphereGeometry(1, 16, 16),
-  //   new THREE.MeshBasicMaterial({ color: 0x633484 })
-  // );
-
-  // scene.add(sphere);
-
-  // function updateRender() {
-  //   renderer.setSize((width.value - 20), height.value);
-  //   renderer.render(scene, camera);
-  // }
-
-  // function updateCamera() {
-  //   camera.aspect = aspectRatio.value;
-  //   camera.updateProjectionMatrix();
-  // }
-
-  // function setRenderer() {
-  //   if(experience.value) {
-  //     renderer = new THREE.WebGLRenderer({ canvas: experience.value, alpha: true });
-  //     updateRender();
-  //   }
-  // }
-
-
-  // watch(aspectRatio, () => {
-  //   updateRender();
-  //   updateCamera();
-  // });
-
-
-  // onMounted(() => {
-  //   setRenderer();
-  // });
-
 </script>
 
 
